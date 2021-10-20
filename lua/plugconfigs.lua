41f181cd2577f4464a2f74c92f07a283b1c83e50
rafidCoding <92775409+rafidCoding@users.noreply.github.com>
2021-10-20 10:58:56 +0600

    updated indent-blankline config

--- lua/plugconfigs.lua
+++ lua/plugconfigs.lua
@@ -402,16 +402,6 @@ end
 
 
 function config.indentBlankline()
-  vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-  vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-  vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-  vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-  vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-  vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-  
-  vim.opt.list = true
-  vim.opt.listchars:append("space:⋅")
-  vim.opt.listchars:append("eol:↴")
   vim.g.indent_blankline_filetype_exclude = {
     "dashboard",
     "dotooagenda",
@@ -434,16 +424,13 @@ function config.indentBlankline()
     "flutterToolsOutline",
     "" -- for all buffers without a file type
   }
+  vim.opt.list = true
+  vim.opt.listchars:append("space:⋅")
+  vim.opt.listchars:append("eol:↴")
+
   require("indent_blankline").setup {
-    space_char_blankline = " ",
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-    },
+      space_char_blankline = " ",
+      show_current_context = true,
   }
 end
 
