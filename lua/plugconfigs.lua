aa2083a255279de1386a175cf74df27441ce061e
rafidCoding <92775409+rafidCoding@users.noreply.github.com>
2021-10-22 13:07:03 +0600

    added galaxyline new theme and whichkey config

--- lua/plugconfigs.lua
+++ lua/plugconfigs.lua
@@ -186,6 +186,20 @@ function config.galaxyline()
       blue = '#839496',
       red = '#dc322f'
     }
+  elseif vim.g.colors_name == "codedark" then
+    colors = {
+      bg = '#808080',
+      fg = '#d4d4d4',
+      yellow = '#dcdcaa',
+      cyan = '#4ec9b0',
+      darkblue = '#569cd6',
+      green = '#608b4e',
+      orange = '#ce9178',
+      violet = '#646695',
+      magenta = '#c586c0',
+      blue = '#9cdcfe',
+      red = '#f44747'
+    }
   else
     colors = require('galaxyline.theme').default
   end
@@ -434,4 +448,30 @@ function config.indentBlankline()
   }
 end
 
+function config.whichkey()
+  local wk = require("which-key")
+  local mappings = {
+    q = {":q<cr>", "Quit"},
+    Q = {":wq<cr>", "Save & Quit"},
+    w = {":w<cr>", "Save"},
+    x = {":bdelete<cr>", "Close"},
+    E = {":e ~/.config/nvim/init.lua<cr>", "Edit config"},
+    f = {":Telescope find_files<cr>", "Telescope Find Files"},
+    r = {":Telescope live_grep<cr>", "Telescope Live Grep"},
+--     l = {
+--       name = "LSP",
+--       i = {":LspInfo<cr>", "Connected Language Servers"},
+--       A = {'<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', "Add workspace folder"},
+--       R = {'<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', "Remove workspace folder"},
+--       l = {'<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', "List workspace folder"},
+--       D = {'<cmd>lua vim.lsp.buf.type_definition()<CR>', "Type definition"},
+--       r = {'<cmd>lua vim.lsp.buf.rename()<CR>', "Rename"},
+--       a = {'<cmd>lua vim.lsp.buf.code_action()<CR>', "Code actions"},
+--       e = {'<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', "Show line diagnostics"},
+--       q = {'<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', "Show loclist"}
+--     },
+  }
+  local opts = {prefix = '<leader>'}
+  wk.register(mappings, opts)
+end
 return config
