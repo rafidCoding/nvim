fd59302e902b246342ca4a3ee534f5ed76875362
rafidCoding <Coding.Rafid@gmail.com>
2021-10-30 06:52:43 +0000

    fixed autopairs issue

--- lua/plugconfigs.lua
+++ lua/plugconfigs.lua
@@ -35,21 +35,26 @@ function config.autopairs()
 
   remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
 
-  require("nvim-autopairs.completion.cmp").setup({
-    map_cr = true, --  map <CR> on insert mode
-    map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-    auto_select = true, -- automatically select the first item
-    insert = false, -- use insert confirm behavior instead of replace
-    map_char = { -- modifies the function or method delimiter by filetypes
-      all = '(',
-      tex = '{'
-    }
-  })
+  -- require("nvim-autopairs.completion.cmp").setup({
+  --   map_cr = true, --  map <CR> on insert mode
+  --   map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
+  --   auto_select = true, -- automatically select the first item
+  --   insert = false, -- use insert confirm behavior instead of replace
+  --   map_char = { -- modifies the function or method delimiter by filetypes
+  --     all = '(',
+  --     tex = '{'
+  --   }
+  -- })
 
 
   local npairs = require("nvim-autopairs")
 
   require('nvim-autopairs').setup{}
+  -- If you want insert `(` after select function or method item
+  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+  local cmp = require('cmp')
+  cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+
 end
 
 
